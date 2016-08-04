23ba8c30c161397a30bdddef55bddd5a45f03fb3
jasonbashov <jbashov@gmail.com>
2016-08-04 20:18:11 +0300

    Added project sceleton

---
+++ AmericanoStatistics/AmericanoStatistics/scripts/app.js
@@ -0,0 +1,15 @@
+﻿requirejs.config({
+    baseUrl: '../scripts',
+    paths: {
+        // the left side is the module ID,
+        // the right side is the path to
+        // the jQuery file, relative to baseUrl.
+        // Also, the path should NOT include
+        // the '.js' file extension. This example
+        // is using jQuery 1.9.0 located at
+        // js/lib/jquery-1.9.0.js, relative to
+        // the HTML page.
+        jquery: 'jquery-3.0.0',
+        indexxx: 'index'
+    }
+});
