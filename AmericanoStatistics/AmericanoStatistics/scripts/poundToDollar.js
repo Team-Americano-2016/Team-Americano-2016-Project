b97b46c979417d0f499fe6d20853ce28b296a592
Ivan <optional303@gmail.com>
2016-08-07 21:32:23 +0300

    Added a new currency button and a new js implementation of the charts(not finished, still work in progress), also added a background image.

---
+++ AmericanoStatistics/AmericanoStatistics/scripts/poundToDollar.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Ivan on 8/7/2016.
+ * Work in progress
+ */
+
+function createChart(type) {
+    var currentType = type.toString();
+    require(['../scripts/libs/chartjs/chart.js'], function (Chart) {
+        var $canvasContainer=$('#char-container').html('');  //clears the previous canvas
+        $('<canvas id="myChart" width="400" height="250" />').appendTo($canvasContainer); //creates a new canvas
+        let ctx = document.getElementById("myChart"),
+            myChart = new Chart(ctx, {
+                type: currentType,
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            });
+
+    });
+
+}
+createChart('box');
