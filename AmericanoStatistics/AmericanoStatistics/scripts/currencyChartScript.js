b428325681d38ab2ff822e8e9c0e3bb46380e4b1
iliangogov <iliangogov@gmail.com>
2016-08-07 14:50:25 +0300

    Clear previous canvas added

--- AmericanoStatistics/AmericanoStatistics/scripts/currencyChartScript.js
+++ AmericanoStatistics/AmericanoStatistics/scripts/currencyChartScript.js
@@ -4,6 +4,8 @@
 function createChart(type) {
     var currentType = type.toString();
         require(['../scripts/libs/chartjs/chart.js'], function (Chart) {
+            var $canvasContainer=$('#char-container').html('');  //clears the previous canvas
+            $('<canvas id="myChart" width="400" height="250" />').appendTo($canvasContainer); //creates a new (clear) canvas
             let ctx = document.getElementById("myChart"),
                 myChart = new Chart(ctx, {
                     type: currentType,
